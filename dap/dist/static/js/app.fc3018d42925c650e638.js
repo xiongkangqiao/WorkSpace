d6896bda19df2b8ed3becc40a20ec71b6d7fe698
xkq <xkq@example.com>
2020-12-22 09:34:57 +0800

    test

---
+++ dap/dist/static/js/app.fc3018d42925c650e638.js
@@ -0,0 +1,172 @@
+webpackJsonp([1], {
+  NHnr: function (t, i, a) {
+    "use strict";
+    Object.defineProperty(i, "__esModule", {value: !0});
+    var e = a("7+uW"), s = {
+      render: function () {
+        var t = this.$createElement;
+        return (this._self._c || t)("router-view")
+      }, staticRenderFns: []
+    };
+    var p = a("VU/8")({name: "App"}, s, !1, function (t) {
+      a("kszH")
+    }, null, null).exports, n = a("/ocq"), c = {
+      name: "Home", filters: {
+        ellipsis_1: function (t) {
+          return t ? t.length > 12 ? t.slice(0, 12) + "..." : t : ""
+        }
+      }, data: function () {
+        return {
+          img_project1: a("mkDz"), number: 8, dataList: {
+            code: "0", msg: "请求成功", data: {
+              records: [{
+                id: "71",
+                title: "中尺度涡旋检测",
+                picUrl: a("mkDz"),
+                status: "未发布",
+                abstractDesc: "中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测",
+                details: "http://www.baidu.com",
+                updateAt: "2020-11-03 11:11:30"
+              }, {
+                id: "71",
+                title: "中尺度涡旋检测",
+                picUrl: " http://127.0.0.1:8889/upload/20201103\\3\\9\\iap.png",
+                status: "未发布",
+                abstractDesc: "中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测",
+                details: "http://www.baidu.com",
+                updateAt: "2020-11-03 11:11:30"
+              }, {
+                id: "71",
+                title: "中尺度涡旋检测",
+                picUrl: " http://127.0.0.1:8889/upload/20201103\\3\\9\\iap.png",
+                status: "未发布",
+                abstractDesc: "中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测",
+                details: "http://www.baidu.com",
+                updateAt: "2020-11-03 11:11:30"
+              }, {
+                id: "71",
+                title: "中尺度涡旋检测",
+                picUrl: " http://127.0.0.1:8889/upload/20201103\\3\\9\\iap.png",
+                status: "未发布",
+                abstractDesc: "中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测",
+                details: "http://www.baidu.com",
+                updateAt: "2020-11-03 11:11:30"
+              }, {
+                id: "71",
+                title: "中尺度涡旋检测",
+                picUrl: " http://127.0.0.1:8889/upload/20201103\\3\\9\\iap.png",
+                status: "未发布",
+                abstractDesc: "中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测",
+                details: "http://www.baidu.com",
+                updateAt: "2020-11-03 11:11:30"
+              }, {
+                id: "71",
+                title: "中尺度涡旋检测",
+                picUrl: " http://127.0.0.1:8889/upload/20201103\\3\\9\\iap.png",
+                status: "未发布",
+                abstractDesc: "中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测",
+                details: "http://www.baidu.com",
+                updateAt: "2020-11-03 11:11:30"
+              }, {
+                id: "71",
+                title: "中尺度涡旋检测",
+                picUrl: " http://127.0.0.1:8889/upload/20201103\\3\\9\\iap.png",
+                status: "未发布",
+                abstractDesc: "中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测",
+                details: "http://www.baidu.com",
+                updateAt: "2020-11-03 11:11:30"
+              }, {
+                id: "71",
+                title: "中尺度涡旋检测",
+                picUrl: " http://127.0.0.1:8889/upload/20201103\\3\\9\\iap.png",
+                status: "未发布",
+                abstractDesc: "中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测中尺度涡旋检测",
+                details: "http://www.baidu.com",
+                updateAt: "2020-11-03 11:11:30"
+              }], total: "1", size: "10", current: "1", orders: [], searchCount: !0, pages: "1"
+            }
+          }
+        }
+      }, methods: {
+        getData: function () {
+          this.axios.get("http://192.168.83.122:8051/web/application/list").then(function (t) {
+            this.dataList = t.data, this.number = this.dataList.data.records.length
+          }.bind(this)).catch(function (t) {
+            t.response && console.log(t.response)
+          }.bind(this))
+        }, jump: function (t) {
+          window.open(t, "_blank")
+        }
+      }, created: function () {
+        this.getData()
+      }
+    }, d = {
+      render: function () {
+        var t = this, i = t.$createElement, a = t._self._c || i;
+        return a("div", [t._m(0), t._v(" "), a("div", {staticClass: "body"}, [t._m(1), t._v(" "), a("div", {
+          staticStyle: {
+            height: "20px",
+            padding: "30px 0",
+            width: "1200px"
+          }
+        }, [a("div", {staticStyle: {"font-size": "20px"}}, [t._v("All Apps (" + t._s(t.number) + ")")])]), t._v(" "), a("div", {
+          staticStyle: {
+            "min-height": "400px",
+            display: "flex",
+            "flex-wrap": "wrap"
+          }
+        }, t._l(t.dataList.data.records, function (i) {
+          return a("div", {
+            staticClass: "project", on: {
+              click: function (a) {
+                return t.jump(i.details)
+              }
+            }
+          }, [a("div", {staticClass: "img"}, [a("img", {
+            staticStyle: {display: "block", margin: "0 auto"},
+            attrs: {src: i.picUrl, alt: "", height: "100%"}
+          }), t._v(" "), a("div", {staticClass: "open"}, [t._v("OPEN")])]), t._v(" "), a("div", {staticClass: "text"}, [a("div", {staticClass: "title"}, [t._v(t._s(t._f("ellipsis_1")(i.title)))])])])
+        }), 0)]), t._v(" "), t._m(2)])
+      }, staticRenderFns: [function () {
+        var t = this.$createElement, i = this._self._c || t;
+        return i("div", {staticClass: "head"}, [i("div", [this._v("海洋领域云应用发布平台")])])
+      }, function () {
+        var t = this.$createElement, i = this._self._c || t;
+        return i("div", {
+          staticStyle: {
+            height: "120px",
+            padding: "20px 0",
+            width: "1200px",
+            "border-bottom": "1px solid #ccc"
+          }
+        }, [i("div", {
+          staticStyle: {
+            width: "360px",
+            height: "100px",
+            float: "left",
+            "font-size": "40px"
+          }
+        }, [this._v("App Gallery")]), this._v(" "), i("div", {staticStyle: {padding: "20px 20px 20px 450px"}}, [this._v("\n海洋领域云应用发布平台\n        ")])])
+      }, function () {
+        var t = this.$createElement, i = this._self._c || t;
+        return i("div", {staticClass: "foot"}, [i("div")])
+      }]
+    };
+    var l = a("VU/8")(c, d, !1, function (t) {
+      a("fCmP")
+    }, "data-v-0d887227", null).exports;
+    e.a.use(n.a);
+    var r = new n.a({routes: [{path: "/", name: "Home", component: l}]}), o = a("mtWM"), u = a.n(o);
+    e.a.prototype.axios = u.a, e.a.config.productionTip = !1, new e.a({
+      el: "#app",
+      router: r,
+      components: {App: p},
+      template: "<App/>"
+    })
+  }, fCmP: function (t, i) {
+  }, kszH: function (t, i) {
+  }, mkDz: function (t, i, a) {
+    t.exports = a.p + "static/img/project_1.206215f.gif"
+  }
+}, ["NHnr"]);
+//# sourceMappingURL=app.fc3018d42925c650e638.js.map
